043a68ee78559e7721c112c8565000663dbfea83
hky <360363511@qq.com>
2025-06-03 15:03:06 +0800

    layer2

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import "@rainbow-me/rainbowkit/styles.css";
 import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { WagmiProvider, http } from "wagmi";
 import { mainnet, arbitrum, optimism, base, zksync } from "wagmi/chains";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AppRoutes from "./routes";
 
 const config = getDefaultConfig({
   transports: {
     [mainnet.id]: http(import.meta.env.VITE_MAINNET_RPC_URL!),
+    [optimism.id]: http(import.meta.env.VITE_OPTIMISM_RPC_URL!),
+    [arbitrum.id]: http(import.meta.env.VITE_ARBITRUM_ONE_RPC_URL!),
+    [base.id]: http(import.meta.env.VITE_BASE_RPC_URL!),
+    [zksync.id]: http(import.meta.env.VITE_ZKSYNC_RPC_URL!),
   },
   appName: "youke",
   projectId: "22048dfbe94bf2f4a260f89c33252adf", // 到 https://cloud.walletconnect.com 注册获取
@@ -19,10 +23,10 @@ const queryClient = new QueryClient();
 export default function App() {
   return (
     <WagmiProvider config={config}>
-       <QueryClientProvider client={queryClient}>
-      <RainbowKitProvider>
-        <AppRoutes />
-      </RainbowKitProvider>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <AppRoutes />
+        </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
   );
