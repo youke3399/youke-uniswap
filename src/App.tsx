77bfc8c90d8512f5fe9cd501a15b25bd33dab098
hq22 <hq@yw.zj.cn>
2025-06-04 21:54:13 +0800

    unichain rpc

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "@rainbow-me/rainbowkit/styles.css";
 import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { WagmiProvider, http } from "wagmi";
-import { mainnet, arbitrum, optimism, base, zksync } from "wagmi/chains";
+import { mainnet, arbitrum, optimism, base, zksync, unichain } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AppRoutes from "./routes";
 
@@ -12,6 +12,7 @@ const config = getDefaultConfig({
     [arbitrum.id]: http(import.meta.env.VITE_ARBITRUM_ONE_RPC_URL!),
     [base.id]: http(import.meta.env.VITE_BASE_RPC_URL!),
     [zksync.id]: http(import.meta.env.VITE_ZKSYNC_RPC_URL!),
+    [unichain.id]: http(import.meta.env.VITE_UNICHAIN_RPC_URL!),
   },
   appName: "youke",
   projectId: "22048dfbe94bf2f4a260f89c33252adf", // 到 https://cloud.walletconnect.com 注册获取
